8353f51d02b12a1f8585f6330037555850a62d38
nguyenhoanganhprograming <2305quynhyeu@gmail.com>
2022-07-23 12:35:15 +0800

    docs

--- vue.config.js
+++ vue.config.js
@@ -2,3 +2,12 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true
 })
+
+
+const path = require("path");
+module.exports = {
+  outputDir: path.resolve(__dirname,"./docs"),
+  publicPath: process.env.NODE_ENV === 'production'
+    ? ''
+    : '/'
+}
